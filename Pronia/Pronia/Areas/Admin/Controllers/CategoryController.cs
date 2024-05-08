f5835649faa10c83e8c8b1729e2a2d67c0669c7a
karimlifatimaa <bd7tzqw95@code.edu.az>
2024-05-08 11:36:36 +0400

    Pronia_WebApp

---
+++ Pronia/Pronia/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Models;
+
+namespace Pronia.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories=_context.Categories.Include(x=>x.Products).ToList();
+            return View(categories);
+        }
+        public IActionResult Delete(int? id)
+        {
+            var category=_context.Categories.FirstOrDefault(x=>x.Id == id);
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Pronia/Pronia/obj/Debug/net6.0/Pronia.AssemblyInfo.cs
+++ Pronia/Pronia/obj/Debug/net6.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c9c1d33745fa80230af0ccd147c8bcb9ed1350")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
